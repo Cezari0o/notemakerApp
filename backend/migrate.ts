e20f6fea835802a03e762bfd9b9f2a5b23c444f4
Gabriel Cesário <gcesario031@gmail.com>
2023-01-04 13:27:27 -0300

    Small fix

--- backend/migrate.ts
+++ backend/migrate.ts
@@ -1,39 +1,3 @@
-// import migrate from 'migrate';
-// import User from './src/users/model';
-// import Note from './src/tags/model';
-// import Tag from './src/notes/model';
-// import fs from  'fs/promises';
-
-// migrate('db-seed', async function (next) {
-//   let users = JSON.parse(await fs.readFile('seed-data/users.json', 'utf-8'));
-//   let tags  = JSON.parse(await fs.readFile('seed-data/tags.json', 'utf-8'));
-//   let notes = JSON.parse(await fs.readFile('seed-data/notes.json', 'utf-8'));
-
-//   await User.insertMany(users);
-//   await Tag.insertMany(tags);
-//   await Note.insertMany(notes);
-
-//   next();
-// }, async function (next) {
-
-//   User.deleteMany({});
-//   Tag.deleteMany({});
-//   Note.deleteMany({});
-
-//   next();
-// });
-
-// const set = migrate();
-
-// set.on('migration', function (migration, direction) {
-//   console.log('%s %s', direction, migration.title);
-// });
-
-// set.up(function (err) {
-//   if (err) throw err;
-//   console.log('migrations successfully ran');
-// });
-
 import migrate from 'migrate';
 import dotenv from 'dotenv';
 
